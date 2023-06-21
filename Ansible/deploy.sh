a1f161cc53522e5a8cb55799aeecfcbb73ae70d7
Roey_Gihan <133655565+Gihan4@users.noreply.github.com>
2023-06-21 19:57:11 +0300

    Update deploy.sh

--- Ansible/deploy.sh
+++ Ansible/deploy.sh
@@ -2,5 +2,5 @@
 sudo yum update -y
 sudo yum install python3 python3-pip -y
 sudo yum install git -y
-sudo yum install ansible -y
-ansible-playbook DevOps-Crypto/Ansible/flask_run.yml
+sudo pip install ansible
+ansible-playbook /home/ec2-user/Ansible/flask_run.yml
