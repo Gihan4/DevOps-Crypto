864f6a5f6426cf0fbeb1c79712b85bb06dd25548
Roey_Gihan <133655565+Gihan4@users.noreply.github.com>
2023-06-24 21:19:58 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# using curl as a test request
+
+testip=$testip
+
 response=$(curl -I -s "http://${testip}:5000/")
 if [[ $response == *"HTTP/1.1 200 OK"* ]]; then
     echo "Curl check successful. App is running."
