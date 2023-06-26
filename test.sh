e3d2d92b4684bb024226a5289f14c5015fb374f5
Roey_Gihan <133655565+Gihan4@users.noreply.github.com>
2023-06-26 09:59:33 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+testip=$(aws ec2 describe-instances --region eu-central-1 --filters 'Name=tag:Environment,Values=Test' --query 'Reservations[].Instances[].PublicIpAddress' --output text | tr -d '[:space:]')
+
 response=$(curl -I -s "http://${testip}:5000/")
 if [[ $response == *"HTTP/1.1 200 OK"* ]]; then
     echo "Curl check successful. App is running."
